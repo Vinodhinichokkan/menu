8c71861c6b8219cb0790bd661e115053c62a6541
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-11-21 12:26:31 +0530

    exe

---
+++ one.py
@@ -0,0 +1,16 @@
+#Build a menu
+
+title = "menu".upper()
+print(title.center(20, "="))
+print("coffee".ljust(16, ".")+ "$1".rjust(4))
+print("Muffin".ljust(16, ".")+ "$2".rjust(4))
+print("Cheesecake".ljust(16, ".")+ "$3".rjust(4))
+
+print("")
+
+#o/p
+
+''''========MENU========
+coffee..........  $1
+Muffin..........  $2
+Cheesecake......  $3'''
